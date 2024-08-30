2f1c54f88dfeb0ef46f54c20d271a227bf18713f
lalitocean <sagarlalit229@gmail.com>
2024-08-30 15:40:18 +0530

    in client side add error and backend side add cors options

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000
 const app = express()
 app.use(cors({
     origin: 'https://sharefilelink.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true,
 }))
 
 app.use('/api/v1', router)
--- client/src/services/upload.js
+++ client/src/services/upload.js
@@ -3,10 +3,9 @@ const apiurl = 'https://file-sharing-taupe.vercel.app/api/v1'
 
 export const uploadfile = async (data) => {
     try {
-
         const response = await axios.post(`${apiurl}/upload`, data)
         return response
     } catch (error) {
-        console.log("i'm inside upload js file ", error.message)
+        console.log("i'm inside upload js file ", error.message, error)
     }
 }
